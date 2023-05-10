fa2b7c93ed38f1220cc7cc6b1cb715145daa12fe
Shashank Chakravarthy <shashank.chakravarthy@gmail.com>
2023-05-10 14:05:22 +0200

    module to identify all references from text

---
+++ references/extract_references.py
@@ -0,0 +1,22 @@
+import os
+
+from legal_openai.openai_tasks import OpenaiTask
+
+
+class ExtractReferences:
+    def __init__(self):
+        self.prompt_path = os.environ.get("PROMPT_PATH")
+
+    def openai_references(self, prompt=None, path=None, api_key=None, article=None):
+        """openai_references.
+
+        Parameters
+        ----------
+        prompt :
+            prompt
+        """
+        if prompt is None:
+            with open(self.prompt_path + 'references.txt', "r") as f:
+                prompt = f.read()
+        return OpenaiTask(path=path,api_key=api_key).execute_task(article=article,
+                                                                  prompt=prompt)
